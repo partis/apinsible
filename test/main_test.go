84de0e5c32b7864af9086d42c239325b9283dc14
partis <andrew_partis@hotmail.com>
2017-05-17 10:30:23 +0100

    restructed and added a simple test

---
+++ test/main_test.go
@@ -0,0 +1,16 @@
+package main_test
+
+import {
+  "testing"
+  "gitub.com/partis/apinsible"
+  "net"
+}
+
+func Test(t *testing.T) {
+  conn, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", 80))
+	if err != nil {
+		t.Error(err.Error)
+	}
+	conn.Close()
+	t.Pass
+}
