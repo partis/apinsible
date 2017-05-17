c5b5d0bb5f64afb80bc6e3cb20d8cb12b69f2cff
partis <andrew_partis@hotmail.com>
2017-05-17 09:54:03 +0100

    added simple server

---
+++ server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "github.com/golang/glog"
+  "os/exec"
+  "strings"
+)
+
+type HasHandleFunc interface { //this is just so it would work for gorilla and http.ServerMux
+    HandleFunc(pattern string, handler func(w http.ResponseWriter, req *http.Request))
+}
+type Handler struct {
+    http.HandlerFunc
+    Enabled bool
+}
+
+type Handlers map[string]*Handler
+
+func (h Handlers) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    path := r.URL.Path
+    if handler, ok := h[path]; ok && handler.Enabled {
+        handler.ServeHTTP(w, r)
+    } else {
+        http.Error(w, "Swagger UI Not Found", http.StatusNotFound)
+    }
+}
+
+func (h Handlers) HandleFunc(mux HasHandleFunc, pattern string, handler http.HandlerFunc) {
+    h[pattern] = &Handler{handler, true}
+    mux.HandleFunc(pattern, h.ServeHTTP)
+}
+
+func swaggerGenerator(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Hello World")
+}
+
+
+func startServer() {
+  http.HandleFunc("/hello_world", helloWorld(swaggerGenerator, contextMap))
+  http.ListenAndServe(":8080", nil)
+  glog.Flush()
+}
